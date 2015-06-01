31a2fbdb4dc038ef42b96d0c855dade1c2be66ed
Yash <yash.saxena1217@gmail.com>
2015-06-01 00:00:41 -0400

    removed adi call that initates scraper

--- public/project/javascripts/controllers/LandingController.js
+++ public/project/javascripts/controllers/LandingController.js
@@ -13,11 +13,4 @@ app.controller('LandingController', [
 	'use strict';
 	console.log('#### Landing Controller');
 
-	$http.get('/api/scrape/cars/2').success(function(response) {
-		console.log(response);
-	});
-
-	socket.on("progress", function(data) {
-		console.log(data);
-	});
 }]);

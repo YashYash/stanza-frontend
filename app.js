97b0468f70b77590407aea775cb11d5a251ddee6
Scott Jason <scottleejason@gmail.com>
2015-06-07 03:17:41 -0700

    pulls master

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });

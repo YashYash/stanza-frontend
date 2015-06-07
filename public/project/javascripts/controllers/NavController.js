e66cd7762f15e219a3f5248ef64eb5a21063a841
Scott Jason <scottleejason@gmail.com>
2015-06-07 13:59:48 -0700

    adds Navbar Ctrl method toggle sidebar on menu opt selected

--- public/project/javascripts/controllers/NavController.js
+++ public/project/javascripts/controllers/NavController.js
@@ -8,23 +8,38 @@ app.controller('NavController', [
     $rootScope,
     $state,
     StateService) {
+
     'use strict';
+
     console.log('#### Nav Controller');
 
-    $scope.goToDesktopRoute = function(route) {
+    var ctrl = this;
+
+    $scope.navigate = function(route) {
+      ctrl.toggleSidebar();
       if (route === 'home') {
         $state.go('app.v1.landing');
       }
-      if (route === 'music') {
+      else if (route === 'music') {
         $state.go('app.v1.music-desktop');
       }
-      if (route === 'booking') {
+      else if (route === 'booking') {
         $state.go('app.v1.booking-desktop');
+      }
+      else if (route === 'cars') {
+        console.log('navigate to cars');
+      }
+      else {
+        console.error('route not found');
       }
     };
 
     $scope.getState = function(container, key) {
       return StateService.data[container][key];
+    };
+
+    ctrl.toggleSidebar = function() {
+      StateService.data['sidebar'].isToggled = !(StateService.data['sidebar'].isToggled);
     };
   }
 ]);
